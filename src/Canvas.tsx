70b0f1fcc7de741127ce9c338ef4ed85f0592c7e
jwu-ice <sju02048@naver.com>
2022-06-30 12:26:09 +0900

    course: before atomfamily

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,18 +1,70 @@
-import {PageContainer} from './PageContainer'
-import {Rectangle} from './components/Rectangle/Rectangle'
-import {Toolbar} from './Toolbar'
+import {createContext, useState} from "react"
+import {Element, Rectangle} from "./components/Rectangle/Rectangle"
+import {PageContainer} from "./PageContainer"
+import {Toolbar} from "./Toolbar"
+import {atom, useSetRecoilState} from "recoil"
+
+type ElementsContextType = {
+  elements: Element[]
+  addElement: () => void
+  setElement: SetElement
+}
+
+export const ElementsContext = createContext<ElementsContextType>({
+  elements: [],
+  addElement: () => {},
+  setElement: () => {},
+})
+
+export const selectedElementState = atom<number | null>({
+  key: "selectedElement",
+  default: null,
+})
+
+export type SetElement = (indexToSet: number, newElement: Element) => void
 
 function Canvas() {
-    return (
-        <PageContainer
-            onClick={() => {
-                console.log('Deselect all elements!')
-            }}
-        >
-            <Toolbar />
-            <Rectangle />
-        </PageContainer>
+  const [elements, setElements] = useState<Element[]>([])
+
+  const setElement: SetElement = (indexToSet, newElement) => {
+    setElements(
+      elements.map((element, index) => {
+        if (indexToSet === index) return newElement
+        return element
+      }),
     )
+  }
+
+  const addElement = () => {
+    setElements((elements) => {
+      return [
+        ...elements,
+        {
+          style: {
+            position: {top: 100 + elements.length * 30, left: 100 + elements.length * 30},
+            size: {width: 100, height: 100},
+          },
+        },
+      ]
+    })
+  }
+
+  const setSelectedElement = useSetRecoilState(selectedElementState)
+
+  return (
+    <ElementsContext.Provider value={{elements, addElement, setElement}}>
+      <PageContainer
+        onClick={() => {
+          setSelectedElement(null)
+        }}
+      >
+        <Toolbar />
+        {elements.map((element, index) => (
+          <Rectangle key={index} element={element} index={index} />
+        ))}
+      </PageContainer>
+    </ElementsContext.Provider>
+  )
 }
 
 export default Canvas
--- src/components/Rectangle/Rectangle.tsx
+++ src/components/Rectangle/Rectangle.tsx
@@ -1,29 +1,44 @@
-import {useState} from 'react'
-import {RectangleContainer} from './RectangleContainer'
-import {RectangleInner} from './RectangleInner'
+import {useContext} from "react"
+import {ElementsContext, selectedElementState} from "../../Canvas"
+import {Drag} from "../Drag"
+import {RectangleContainer} from "./RectangleContainer"
+import {RectangleInner} from "./RectangleInner"
+import {useRecoilState} from "recoil"
 
 export type ElementStyle = {
-    position: {top: number; left: number}
-    size: {width: number; height: number}
+  position: {top: number; left: number}
+  size: {width: number; height: number}
 }
 
-export const Rectangle = () => {
-    const [element] = useState({
-        style: {
-            position: {top: 100, left: 100},
-            size: {width: 100, height: 100},
-        },
-    })
+export type Element = {style: ElementStyle}
 
-    return (
+export const Rectangle = ({element, index}: {element: Element; index: number}) => {
+  const [selectedElement, setSelectedElement] = useRecoilState(selectedElementState)
+  const {setElement} = useContext(ElementsContext)
+
+  return (
+    <Drag
+      position={element.style.position}
+      onDrag={(position) => {
+        setElement(index, {
+          style: {
+            ...element.style,
+            position,
+          },
+        })
+      }}
+    >
+      <div>
         <RectangleContainer
-            position={element.style.position}
-            size={element.style.size}
-            onSelect={() => {
-                console.log("I've been selected!")
-            }}
+          position={element.style.position}
+          size={element.style.size}
+          onSelect={() => {
+            setSelectedElement(index)
+          }}
         >
-            <RectangleInner selected={false} />
+          <RectangleInner selected={index === selectedElement} />
         </RectangleContainer>
-    )
+      </div>
+    </Drag>
+  )
 }
